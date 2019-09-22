14bf64d60b3c5a42fa5586c9fef3f9591c9bb1c9
Anurag Gupta <anurag.iiits@gmail.com>
2019-09-22 20:17:15 +0530

    Project Finished

---
+++ executor-app/execute_event.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.execute = (event, context, callback) => {
+  
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.id,
+    },
+  };
+
+  // fetch event from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the item.',
+      });
+      return;
+    }
+
+    //Make API call
+    console.log('Sending ' + result.Item.method + ' request to ' + result.Item.url)
+
+    const myURL = new URL(result.Item.url);
+    var dataString = JSON.stringify(result.Item.payload)
+
+    const options = {
+      host: myURL.host,
+      path: myURL.pathname,
+      method: result.Item.method,
+      headers: result.Item.headers,
+      rejectUnauthorized: false
+    }
+
+    var req = https.request(options, function (res) {
+      res.setEncoding('utf-8')
+
+      var responseString = ''
+
+      res.on('data', function (data) {
+        responseString += data
+      })
+
+      res.on('end', function () {
+        console.log(responseString)
+      })
+    })
+
+    req.write(dataString)
+    req.end()
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
---
+++ schedule-app/schedule_event.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const stepfunctions = new AWS.StepFunctions();
+
+module.exports.schedule = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.method !== 'string' || typeof data.url !== 'string' || typeof data.execution_time !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the event.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      url: data.url,
+      method: data.method,
+      payload: data.payload,
+      headers: data.headers,
+      execution_time: data.execution_time,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the event to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the event.',
+      });
+      return;
+    }
+
+    const stateMachineArn = process.env.statemachine_arn;
+    const input = {
+            "id": params.Item.id,             // Only passing the unique Event ID from this function to state machine instead of passing all the API data
+                                              // This approach can be better because we can also update the data (except execution_time) for this event later by 
+                                              // directly making changes in database and we do not need to stop/change anything in the step functions
+            "execution_time": params.Item.execution_time
+    }
+    const stateMachineParams = {
+        stateMachineArn,
+        input: json.dumps(input, indent=4)
+    }
+
+    stepfunctions.startExecution(stateMachineParams).promise().then(() => {
+        console.log(`Your statemachine ${stateMachineArn} executed successfully`);
+    }).catch(error => {
+        callback(error.message);
+    });
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
