84c403ccadeb207dcb5c4429d311794804d4bcdc
Anurag Gupta <anurag.iiits@gmail.com>
2019-09-23 03:47:06 +0530

    Added Linters and Other Enhancements

--- executor-app/execute_event.js
+++ executor-app/execute_event.js
@@ -1,38 +1,38 @@
-'use strict';
+'use strict'
 
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
-const https = require('https');
-const http = require('http');
+const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-dependencies
+const https = require('https')
+const http = require('http')
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
+// This function is responsibe for executing the events
 module.exports.execute = (event, context, callback) => {
-  
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Key: {
-      id: event.id,
-    },
-  };
+      id: event.id
+    }
+  }
 
   // fetch event from the database
   dynamoDb.get(params, (error, result) => {
     // handle potential errors
     if (error) {
-      console.error(error);
+      console.error(error)
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the item.',
-      });
-      return;
+        body: 'Couldn\'t fetch the item.'
+      })
+      return
     }
 
-    //Make API call
+    // Make API call
     console.log('Sending ' + result.Item.method + ' request to ' + result.Item.url)
 
-    const myURL = new URL(result.Item.url);
-    const dataString = (result.Item.payload !== undefined) ? JSON.stringify(result.Item.payload) : ""; 
+    const myURL = new URL(result.Item.url)
+    const dataString = (result.Item.payload !== undefined) ? JSON.stringify(result.Item.payload) : ''
 
     const options = {
       host: myURL.host,
@@ -41,12 +41,13 @@ module.exports.execute = (event, context, callback) => {
       rejectUnauthorized: false
     }
 
-    if(result.Item.headers !== undefined){
-      options.headers = JSON.stringify(result.Item.headers);
+    if (result.Item.headers !== undefined) {      // Because in some cases, there might not be headers attribute in the object
+      options.headers = JSON.stringify(result.Item.headers)
     }
 
-    if(myURL.protocol === "http:"){
-      var req = http.request(options, function (res) {
+    var req
+    if (myURL.protocol === 'http:') {             // If the protocol of the API to be called is http
+      req = http.request(options, function (res) {
         res.setEncoding('utf-8')
         var responseString = ''
 
@@ -57,14 +58,8 @@ module.exports.execute = (event, context, callback) => {
           console.log(responseString)
         })
       })
-
-      if(result.Item.method !== 'GET' && result.Item.method !== 'HEAD'){
-        req.write(dataString)
-      }
-      req.end()
-    }
-    else{         //else it is https
-      var req = https.request(options, function (res) {
+    } else {                                      // else it is https
+      req = https.request(options, function (res) {
         res.setEncoding('utf-8')
         var responseString = ''
 
@@ -75,17 +70,17 @@ module.exports.execute = (event, context, callback) => {
           console.log(responseString)
         })
       })
-
+    }
+    if (result.Item.method !== 'GET' && result.Item.method !== 'HEAD') {
       req.write(dataString)
-      req.end()
     }
-    
+    req.end()
 
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Item),
-    };
-    callback(null, response);
-  });
-};
+      body: JSON.stringify(result.Item)
+    }
+    callback(null, response)
+  })
+}
--- schedule-app/schedule_event.js
+++ schedule-app/schedule_event.js
@@ -1,31 +1,33 @@
-'use strict';
+'use strict'
 
-const uuid = require('uuid');
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const uuid = require('uuid')
+const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-dependencies
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const stepfunctions = new AWS.StepFunctions();
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const stepfunctions = new AWS.StepFunctions()
 
 module.exports.schedule = (event, context, callback) => {
-  const timestamp = new Date().getTime();
-  const data = JSON.parse(event.body);
+  const timestamp = new Date().getTime()
+  const data = JSON.parse(event.body)
+
+  // Validation of Request Data
   if (typeof data.method !== 'string' || typeof data.url !== 'string' || typeof data.execution_time !== 'string') {
-    console.error('Validation Failed');
+    console.error('Validation Failed')
     callback(null, {
       statusCode: 400,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Couldn\'t create the event. Incomplete Data Sent.',
-    });
-    return;
+      body: 'Couldn\'t create the event. Incomplete Data Sent.'
+    })
+    return
   }
   if (data.method !== 'GET' && data.method !== 'POST' && data.method !== 'PUT' && data.method !== 'PATCH' && data.method !== 'DELETE' && data.method !== 'HEAD') {
-    console.error('Validation Failed');   // Assuming that only these http methods are allowed
+    console.error('Validation Failed') // Assuming that only these http methods are allowed
     callback(null, {
       statusCode: 400,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Http Method is not allowed or invalid.',
-    });
-    return;
+      body: 'Http Method is not allowed or invalid.'
+    })
+    return
   }
 
   const params = {
@@ -36,14 +38,14 @@ module.exports.schedule = (event, context, callback) => {
       method: data.method,
       execution_time: data.execution_time,
       createdAt: timestamp,
-      updatedAt: timestamp,
-    },
-  };
+      updatedAt: timestamp
+    }
+  }
 
-  if(typeof data.payload === 'string' && data.payload.length > 0){   // because dynamodb doesn't accept empty string through this aws-sdk version.
+  if (typeof data.payload === 'string' && data.payload.length > 0) { // because dynamodb doesn't accept empty string through this aws-sdk version.
     params.Item.payload = data.payload
   }
-  if(typeof data.headers === 'string' && data.payload.headers > 0){   // because dynamodb doesn't accept empty string through this aws-sdk version.
+  if (typeof data.headers === 'string' && data.payload.headers > 0) { // because dynamodb doesn't accept empty string through this aws-sdk version.
     params.Item.headers = data.headers
   }
 
@@ -51,39 +53,39 @@ module.exports.schedule = (event, context, callback) => {
   dynamoDb.put(params, (error) => {
     // handle potential errors
     if (error) {
-      console.error("Couldn\'t insert", error);
+      console.error('Couldn\'t insert', error)
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the event.',
-      });
-      return;
+        body: 'Couldn\'t create the event.'
+      })
+      return
     }
 
-    const stateMachineArn = process.env.statemachine_arn;
+    const stateMachineArn = process.env.statemachine_arn
     const input = {
-            "id": params.Item.id,             // Only passing the unique Event ID from this function to state machine instead of passing all the API data
-                                              // This approach can be better because we can also update the data (except execution_time) for this event later by 
-                                              // directly making changes in database and we do not need to stop/change anything in the step functions
-            "execution_time": params.Item.execution_time
+      id: params.Item.id, // Only passing the unique Event ID from this function to state machine instead of passing all the API data
+      // This approach can be better because we can also update the data (except execution_time) for this event later by
+      // directly making changes in database and we do not need to stop/change anything in the step functions
+      execution_time: params.Item.execution_time
     }
     const stateMachineParams = {
-        stateMachineArn,
-        "input": JSON.stringify(input)
+      stateMachineArn,
+      input: JSON.stringify(input)
     }
 
-    stepfunctions.startExecution(stateMachineParams).promise().then(() => {
-        console.log(`Your statemachine ${stateMachineArn} executed successfully`);
+    stepfunctions.startExecution(stateMachineParams).promise().then(() => {       // Starts the Execution of State Machine
+      console.log(`Your statemachine ${stateMachineArn} executed successfully`)
     }).catch(error => {
-        console.error(`Your statemachine ${stateMachineArn} failed`, error)
-        callback(error.message);
-    });
+      console.error(`Your statemachine ${stateMachineArn} failed`, error)
+      callback(error.message)
+    })
 
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
-  });
-};
+      body: JSON.stringify(params.Item)
+    }
+    callback(null, response)
+  })
+}
